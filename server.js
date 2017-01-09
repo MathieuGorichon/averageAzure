cfc995fed1510b337f977f2263d84cab03ab4f36
Mathieu Gorichon <m.gorichon@lectra.com>
2017-01-09 16:17:07 +0100

    add normalize port

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const account = 'mgn'
 const access_key = 'fWFWgqF9rQorp/iGlg05tRnV+8iHnbF8ANCGTDjFJxJSKIUMVzGEU9rq9GhGgZCy+m2+ubZxX1AbFEguIYX+YQ=='
 
+app.get('/', (req, res) => res.send('Running'));
+
 app.post('/add/:message', function (req, res) {
 	var queueService = azure.createQueueService(account, access_key);
 	queueService.createQueueIfNotExists('taskqueue', function(error) {
@@ -36,6 +38,18 @@ app.get('/getmessage', function(req, res) {
 	});
 });
 
-app.listen(3000, function () {
-  console.log('Running on port 3000!');
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
+
+app.listen( normalizePort(process.env.PORT || '3000'), function () {
+  console.log('Running on port ' + normalizePort(process.env.PORT || '3000'));
 });
